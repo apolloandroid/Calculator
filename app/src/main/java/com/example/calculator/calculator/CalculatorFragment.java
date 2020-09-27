38a7e4157c672cdb6191135ce14480b211ed37a5
apolloandroid <hmelevskih94@gmail.com>
2020-09-27 18:07:53 +0300

    Initial commit

--- app/src/main/java/com/example/calculator/calculator/CalculatorFragment.java
+++ app/src/main/java/com/example/calculator/calculator/CalculatorFragment.java
@@ -1,26 +1,160 @@
 package com.example.calculator.calculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import com.example.calculator.R;
+import com.example.calculator.databinding.FragmentCalculatorBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 
-public class CalculatorFragment extends Fragment {
+public class CalculatorFragment extends Fragment implements View.OnClickListener {
+    private FragmentCalculatorBinding binding;
+    private CalculatorViewModel viewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calculator, container, false);
+        viewModel = new CalculatorViewModel();
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_calculator, container, false);
+        binding.setLifecycleOwner(this);
+        binding.setViewModel(viewModel);
+        setOnClickListeners();
+        initObservers();
+
+        return binding.getRoot();
+    }
+
+    private void initObservers() {
+        viewModel.getIncorrectExpression().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean)
+                    Snackbar.make(binding.getRoot(), "Incorrect expression", Snackbar.LENGTH_LONG);
+            }
+        });
+
+        viewModel.getNavigateToSecretScreen().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean) navigateToSecretScreen();
+            }
+        });
+    }
+
+    private void setOnClickListeners() {
+        binding.buttonAdd.setOnClickListener(this);
+        binding.buttonSubtract.setOnClickListener(this);
+        binding.buttonMultiply.setOnClickListener(this);
+        binding.buttonDivide.setOnClickListener(this);
+        binding.buttonOpenBracket.setOnClickListener(this);
+        binding.buttonCloseBracket.setOnClickListener(this);
+        binding.buttonZero.setOnClickListener(this);
+        binding.buttonOne.setOnClickListener(this);
+        binding.buttonTwo.setOnClickListener(this);
+        binding.buttonThree.setOnClickListener(this);
+        binding.buttonFour.setOnClickListener(this);
+        binding.buttonFive.setOnClickListener(this);
+        binding.buttonSix.setOnClickListener(this);
+        binding.buttonSeven.setOnClickListener(this);
+        binding.buttonEight.setOnClickListener(this);
+        binding.buttonNine.setOnClickListener(this);
+        binding.buttonDot.setOnClickListener(this);
+        binding.buttonDelete.setOnClickListener(this);
+        binding.buttonClear.setOnClickListener(this);
+//        binding.buttonCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String buttonText = "";
+        switch (v.getId()) {
+            case R.id.button_add:
+                buttonText = binding.buttonAdd.getText().toString();
+                break;
+            case R.id.button_subtract:
+                buttonText = binding.buttonSubtract.getText().toString();
+                break;
+            case R.id.button_multiply:
+                buttonText = binding.buttonMultiply.getText().toString();
+                break;
+            case R.id.button_divide:
+                buttonText = binding.buttonDivide.getText().toString();
+                break;
+            case R.id.button_open_bracket:
+                buttonText = binding.buttonOpenBracket.getText().toString();
+                break;
+            case R.id.button_close_bracket:
+                buttonText = binding.buttonCloseBracket.getText().toString();
+                break;
+            case R.id.button_zero:
+                buttonText = binding.buttonZero.getText().toString();
+                break;
+            case R.id.button_one:
+                buttonText = binding.buttonOne.getText().toString();
+                break;
+            case R.id.button_two:
+                buttonText = binding.buttonTwo.getText().toString();
+                break;
+            case R.id.button_three:
+                buttonText = binding.buttonThree.getText().toString();
+                break;
+            case R.id.button_four:
+                buttonText = binding.buttonFour.getText().toString();
+                break;
+            case R.id.button_five:
+                buttonText = binding.buttonFive.getText().toString();
+                break;
+            case R.id.button_six:
+                buttonText = binding.buttonSix.getText().toString();
+                break;
+            case R.id.button_seven:
+                buttonText = binding.buttonSeven.getText().toString();
+                break;
+            case R.id.button_eight:
+                buttonText = binding.buttonEight.getText().toString();
+                break;
+            case R.id.button_nine:
+                buttonText = binding.buttonNine.getText().toString();
+                break;
+            case R.id.button_dot:
+                buttonText = binding.buttonDot.getText().toString();
+                break;
+            case R.id.button_delete:
+                viewModel.deleteSymbol();
+                break;
+            case R.id.button_clear:
+                viewModel.clear();
+                break;
+            case R.id.button_calculate:
+                viewModel.calculate();
+                break;
+        }
+
+        viewModel.addSymbol(buttonText);
+    }
+
+    private void navigateToSecretScreen() {
+        if (Navigation.findNavController(binding.getRoot()).getCurrentDestination().getId() == R.id.calculatorFragment) {
+            Navigation.findNavController(binding.getRoot()).navigate(R.id.action_calculatorFragment_to_secretFragment);
+        }
     }
 }
--- app/src/main/java/com/example/calculator/calculator/CalculatorViewModel.java
+++ app/src/main/java/com/example/calculator/calculator/CalculatorViewModel.java
@@ -1,4 +1,88 @@
 package com.example.calculator.calculator;
 
-class CalculatorViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import java.util.Objects;
+
+public class CalculatorViewModel extends ViewModel {
+    private final String PASSWORD = "123";
+    private boolean isSecretMode = false;
+    private String secretModeExpression = "";
+
+    private MutableLiveData<String> expression = new MutableLiveData<>("");
+
+    public MutableLiveData<String> getExpression() {
+        return expression;
+    }
+
+    private MutableLiveData<String> result = new MutableLiveData<>("");
+
+    public MutableLiveData<String> getResult() {
+        return result;
+    }
+
+    private MutableLiveData<Boolean> incorrectExpression = new MutableLiveData<>(false);
+
+    public MutableLiveData<Boolean> getIncorrectExpression() {
+        return incorrectExpression;
+    }
+
+    private MutableLiveData<Boolean> navigateToSecretScreen = new MutableLiveData<>(false);
+
+    public MutableLiveData<Boolean> getNavigateToSecretScreen() {
+        return navigateToSecretScreen;
+    }
+
+    public void addSymbol(String text) {
+        if (isSecretMode) {
+            secretModeExpression += text;
+            expression.setValue(expression.getValue() + text);
+        }
+        expression.setValue(expression.getValue() + text);
+    }
+
+    public void deleteSymbol() {
+        if (Objects.requireNonNull(expression.getValue()).length() > 0)
+            expression.setValue(expression.getValue().substring(0, expression.getValue().length() - 1));
+    }
+
+    public void clear() {
+        expression.setValue("");
+        result.setValue("");
+    }
+
+    public void calculate() {
+        if (!isSecretMode) {
+            try {
+                Expression expressionBuilder = new ExpressionBuilder(Objects.requireNonNull(expression.getValue())).build();
+                double expressionResult = expressionBuilder.evaluate();
+                result.setValue(String.valueOf(expressionResult));
+            } catch (Exception e) {
+                incorrectExpression.setValue(true);
+            }
+        } else {
+
+        }
+
+
+    }
+
+    public void turnSecretModeOn() {
+        isSecretMode = true;
+    }
+
+    public void turnSecretModeOff() {
+        isSecretMode = false;
+        navigateToSecretScreen.setValue(false);
+        secretModeExpression = "";
+
+    }
+
+    public void checkSecretPassword() {
+        if (secretModeExpression.equals(PASSWORD)) navigateToSecretScreen.setValue(true);
+    }
 }
