5ee5fe09e6bfbd949811de1db2dd38b74f88ed24
apolloandroid <hmelevskih94@gmail.com>
2020-09-29 01:22:24 +0300

    Component for Dependency Injection

---
+++ app/src/main/java/com/example/calculator/di/AppComponent.java
@@ -0,0 +1,13 @@
+package com.example.calculator.di;
+
+import com.example.calculator.calculator.CalculatorFragment;
+
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+@Singleton
+@Component(modules = CalculatorFragmentModule.class)
+public interface AppComponent {
+    void injectCalculatorFragment(CalculatorFragment calculatorFragment);
+}
