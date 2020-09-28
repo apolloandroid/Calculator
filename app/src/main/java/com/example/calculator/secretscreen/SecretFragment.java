41ef0a2a1113b116896eb085517db551203135ca
apolloandroid <hmelevskih94@gmail.com>
2020-09-29 01:15:14 +0300

    Secret screen

--- app/src/main/java/com/example/calculator/secretscreen/SecretFragment.java
+++ app/src/main/java/com/example/calculator/secretscreen/SecretFragment.java
@@ -1,7 +1,10 @@
 package com.example.calculator.secretscreen;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,18 +12,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.calculator.R;
+import com.example.calculator.databinding.FragmentSecretBinding;
 
 
 public class SecretFragment extends Fragment {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_secret, container, false);
+
+        FragmentSecretBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_secret, container, false);
+
+        binding.toolbar.getNavigationIcon().setColorFilter(
+                ContextCompat.getColor(requireContext(), R.color.navigationButtonColor),
+                PorterDuff.Mode.SRC_ATOP);
+
+        binding.toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().onBackPressed();
+            }
+        });
+        return binding.getRoot();
     }
 }
